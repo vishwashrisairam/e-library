5289d3302ef4d89aabe19023142565f8f2406c09
vishwashrisairam <vishwashrisairam@gmail.com>
2016-01-19 19:37:01 +0530

    final commit

---
+++ png/urban_files/deckNP_js.php
@@ -0,0 +1,17 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<a href="http://www.pixate.com?deck">';
+advert+='<img class="ad" alt="Pixate" src="//connect.decknetwork.net/i/pixate2.png" border="0" width="120" height="90" />';
+advert+='</a>';
+advert+='<p class="ads">';
+advert+='Visually design native mobile prototypes. Free plan <a href="http://www.pixate.com?deck">now available!</a>';
+advert+='</p>';
+
+
+
+
+document.write(advert);
+
+
